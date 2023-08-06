d181022cfd91902768cbbaf9a257206bb4180010
AVS7777 <arturo02nov@gmail.com>
2023-08-06 16:24:37 -0600

    MVP Blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,19 @@
   Rails.application.routes.draw do
+   
     resources :categories
     devise_for :users
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
+    get "profile", to: "users#edit"
+    resources :users, only: [:update]
   
-  root to:"home#index" 
+    root to:"home#index" 
 
-  resources :articles do 
-    get "user/:id", to: "articles#from_author", on: :collection 
-  end
+    resources :articles do 
+      get "user/:id", to: "articles#from_author", on: :collection 
+    end
   
   # get "articles", to: "articles#index"
   # get "articles/new", to:"articles#new",  as: :new_articles
